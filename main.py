ae6ca3b556562280d19e81bb6358fa2ab0a7a7f7
David <53013129+Davdeveloper933@users.noreply.github.com>
2024-01-29 04:37:18 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from pydub import AudioSegment
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledapplicationFlow
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.auth.transport.requests import Request
